6df7f40d7edf83615cf58ebf92e23de98cd280ed
DeAlexPesh <DeAlexWin@gmail.com>
2018-06-28 10:44:10 +0500

    add Russian translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -9,6 +9,7 @@ import zhCN from './zh-cn.yaml'
 import zhTW from './zh-tw.yaml'
 import es from './es.yaml'
 import de from './de.yaml'
+import ru from './ru.yaml'
 
 Vue.use(VueI18n)
 
@@ -45,6 +46,9 @@ export function detectLocale () {
     case /^de.*/i.test(locale):
       locale = 'de'
       break
+    case /^ru.*/i.test(locale):
+      locale = 'ru'
+      break
     default:
       locale = 'en'
   }
@@ -65,6 +69,7 @@ const i18n = new VueI18n({
     'zh-tw': zhTW,
     'es': es,
     'de': de
+    'ru': ru
   }
 })
 
